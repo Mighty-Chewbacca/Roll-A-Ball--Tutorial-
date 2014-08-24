9b1ff223d1ebc629bce3d9a3d96dbcc0d6d16366
Mighty-Chewbacca <alexmalcolm93@live.co.uk>
2014-08-25 00:22:02 +0100

    Parts 2 and 3 of ball game
    added player movement
    added camera movement
    added walls to stop player falling off sides

---
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//script from unity tutorials, used to make the ball move!
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+}
